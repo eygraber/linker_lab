3e1760114bdaa09acef11873dc2366ec4b8fa59b
Eliezer Graber <eygraber@gmail.com>
2013-02-11 15:17:38 -0500

    Added comments and delete GlobalState pointer when finished processing

--- main.cpp
+++ main.cpp
@@ -7,11 +7,14 @@
 using namespace std;
 
 int main(int argc, char** argv) {
+    //temporarily sets stdout to ./output.txt
     ofstream out("output.txt");
     streambuf* coutbuf = cout.rdbuf();
     cout.rdbuf(out.rdbuf());
+    //for all of the filenames passed in
     for(int i = 1; i < argc; i++) {
         cout << "--------For File " << argv[i] << "---------" << endl;
+        //create a new GlobalState for each file
         GlobalState* gs = new GlobalState();
 
             try {
@@ -22,11 +25,16 @@ int main(int argc, char** argv) {
                 gs->printAddressList(modules);
                 gs->printNotUsedWarnings();
             } catch(LinkerException e) {
+                //prints the error
                 cout << e.what() << endl;
             }
+
+        delete gs;
+
         cout << endl;
     }
 
+    //sets stdout back
     cout.rdbuf(coutbuf);
 
     return 0;
